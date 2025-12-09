95734a3dce98b5da637fbf8465af65451b3a1aa6
Aman Roy <royaman56456@gmail.com>
2025-12-09 09:40:09 +0530

    Committing Day-65 (Python Call by value and call by refernce)

---
+++ Python_CallByValue_CallByReference/A3.py
@@ -0,0 +1,30 @@
+# Immutable-> int
+# Mutable-> list
+
+#  Using int
+# def f1(a):
+#     print(id(a))
+#     a+=1
+#     print(id(a))
+#     print(a)
+
+
+# b=5
+# print(id(b))
+# f1(b)
+# print(b)
+
+
+# Using list
+# def f1(a):
+#     print(id(a))
+#     a[0]=10
+#     print(id(a))
+#     print(a)
+
+
+# b=[1,2,3]
+# print(id(b))
+# f1(b)
+# print(b)
+
